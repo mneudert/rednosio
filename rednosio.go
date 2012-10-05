0767979084d7dcca7307dbabcd40cec000053f02
Marc Neudert <marc.neudert@gmail.com>
2012-10-05 23:35:54 +0200

    creates thumbs for browse page images

--- rednosio.go
+++ rednosio.go
@@ -7,15 +7,16 @@ import (
 
 func main() {
     http.HandleFunc("/", handlers.Error(handlers.Index))
-    http.HandleFunc("/browse/downloads", handlers.Error(handlers.BrowseDownloads));
-    http.HandleFunc("/browse/uploads", handlers.Error(handlers.BrowseUploads));
+    http.HandleFunc("/browse/downloads", handlers.Error(handlers.BrowseDownloads))
+    http.HandleFunc("/browse/uploads", handlers.Error(handlers.BrowseUploads))
     http.HandleFunc("/image", handlers.Error(handlers.Image))
     http.HandleFunc("/rednosify", handlers.Error(handlers.Rednosify))
     http.HandleFunc("/save", handlers.Error(handlers.SaveImage))
+    http.HandleFunc("/thumb/download", handlers.Error(handlers.DownloadThumb))
+    http.HandleFunc("/thumb/upload", handlers.Error(handlers.UploadThumb))
 
     http.Handle("/downloads/", http.StripPrefix("/downloads/", http.FileServer(http.Dir("./downloads"))))
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-    http.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
 
     http.ListenAndServe(":8080", nil)
 }
---
+++ rednosio/handlers/thumb.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+    "image"
+    "image/png"
+    "net/http"
+    "os"
+
+    "github.com/nfnt/resize"
+)
+
+func DownloadThumb(w http.ResponseWriter, r *http.Request) {
+    f, err := os.Open("thumbs/downloads/" + r.FormValue("id") + ".png")
+    if nil != err {
+        createThumb("downloads", r.FormValue("id"))
+
+        f, err = os.Open("thumbs/downloads/" + r.FormValue("id") + ".png")
+        if nil != err { return }
+    }
+
+    i, _, err := image.Decode(f)
+    if nil != err { return; }
+
+    w.Header().Set("Content-type", "image/png")
+    png.Encode(w, i)
+}
+
+func UploadThumb(w http.ResponseWriter, r *http.Request) {
+    f, err := os.Open("thumbs/uploads/" + r.FormValue("id") + ".png")
+    if nil != err {
+        createThumb("uploads", r.FormValue("id"))
+
+        f, err = os.Open("thumbs/uploads/" + r.FormValue("id") + ".png")
+        if nil != err { return }
+    }
+
+    i, _, err := image.Decode(f)
+    if nil != err { return; }
+
+    w.Header().Set("Content-type", "image/png")
+    png.Encode(w, i)
+}
+
+func createThumb(folder, id string) {
+    n, err := os.Open(folder + "/" + id + ".png")
+    if nil != err { return }
+
+    i, _, err := image.Decode(n)
+    if nil != err { return }
+
+    ires := resize.Resize(140, 0, i, resize.Lanczos3)
+
+    nres, err := os.OpenFile("thumbs/" + folder + "/" + id + ".png", os.O_RDWR | os.O_CREATE, 0666)
+    if nil != err { return }
+
+    png.Encode(nres, ires)
+}
