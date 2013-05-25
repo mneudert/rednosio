3c478e7bc698f8b3e599f5a5709e3270e0fa9023
Marc Neudert <marc.neudert@gmail.com>
2013-05-25 15:58:14 +0200

    runs http handler in goroutine

--- rednosio.go
+++ rednosio.go
@@ -6,6 +6,7 @@ import (
     "net/http"
     "os"
     "strconv"
+    "sync"
 
     "./rednosio/handlers"
 )
@@ -18,19 +19,28 @@ func main() {
         port, _ = strconv.Atoi(args[2]);
     }
 
+
+    wg := &sync.WaitGroup{}
+
+    wg.Add(1)
+    go func() {
+        http.HandleFunc("/", handlers.Error(handlers.Index))
+        http.HandleFunc("/browse/downloads", handlers.Error(handlers.BrowseDownloads))
+        http.HandleFunc("/browse/uploads", handlers.Error(handlers.BrowseUploads))
+        http.HandleFunc("/image", handlers.Error(handlers.Image))
+        http.HandleFunc("/rednosify", handlers.Error(handlers.Rednosify))
+        http.HandleFunc("/save", handlers.Error(handlers.SaveImage))
+        http.HandleFunc("/thumb/download", handlers.Error(handlers.DownloadThumb))
+        http.HandleFunc("/thumb/upload", handlers.Error(handlers.UploadThumb))
+
+        http.Handle("/downloads/", http.StripPrefix("/downloads/", http.FileServer(http.Dir("./downloads"))))
+        http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+        http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+        wg.Done()
+    }()
+
     log.Printf("Listening at \":%d\"\n", port)
-
-    http.HandleFunc("/", handlers.Error(handlers.Index))
-    http.HandleFunc("/browse/downloads", handlers.Error(handlers.BrowseDownloads))
-    http.HandleFunc("/browse/uploads", handlers.Error(handlers.BrowseUploads))
-    http.HandleFunc("/image", handlers.Error(handlers.Image))
-    http.HandleFunc("/rednosify", handlers.Error(handlers.Rednosify))
-    http.HandleFunc("/save", handlers.Error(handlers.SaveImage))
-    http.HandleFunc("/thumb/download", handlers.Error(handlers.DownloadThumb))
-    http.HandleFunc("/thumb/upload", handlers.Error(handlers.UploadThumb))
-
-    http.Handle("/downloads/", http.StripPrefix("/downloads/", http.FileServer(http.Dir("./downloads"))))
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+    log.Println("CTRL-C to exit...")
+    wg.Wait()
 }
